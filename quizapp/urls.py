697723fc67de01e51f14a9489aec9f91b29c4e60
Nithin-at-github <104214308+Nithin-at-github@users.noreply.github.com>
2022-04-26 23:06:04 +0530

    Updated urls

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from users import views as users_views
 from site_admin import views as admin_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.static import serve
 
 urlpatterns = [
