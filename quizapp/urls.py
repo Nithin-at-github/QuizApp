c4e957cc508da303d998a649e87c691d95fa9032
Nithin-at-github <104214308+Nithin-at-github@users.noreply.github.com>
2022-04-26 22:57:01 +0530

    updated urls

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -14,18 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from users import views as users_views
 from site_admin import views as admin_views
 
 from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
     path('', users_views.home, name='home'),
     path('signup', users_views.signup, name='signup'),
